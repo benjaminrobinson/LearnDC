19dccc71086a28a7e2bb00303385c6025d5cec5d
benjaminrobinson <benj.robinson2@gmail.com>
2016-04-04 11:32:59 -0400

    Removed start up message file because it was causing the package installation to fail

--- R/start_up_message.R
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(...){
-return(packageStartupMessage("Welcome to the development version of the LearnDC R Package! If you encounter a clear bug, please file a minimal reproducible example at the package repository: https://github.com/benjaminrobinson/LearnDC/issues. Also, please feel free to email the package creator with questions at benj.robinson2@gmail.com."))
-}
