7ddb63bcb32a42f3a4cdb39b9535587c0afc66bc
Ben Robinson <benjaminrobinson@users.noreply.github.com>
2016-08-09 16:31:03 -0400

    Change "eligible" to "Eligible"

--- R/GetSchool.R
+++ R/GetSchool.R
@@ -52,7 +52,7 @@ Please check the spelling of your exhibit using GetExhibits('school') to get the
                       "mu7"="Multiracial",
                       "pi7"="Pacific Islander",
                       "am7"="American Indian",
-                      "direct cert"="TANF/SNAP eligible",
+                      "direct cert"="TANF/SNAP Eligible",
                       "economy"="Economically Disadvantaged",
                       "lep"="English Learner",
                       "sped"="Special Education",
