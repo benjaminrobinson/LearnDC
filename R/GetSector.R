735bcbe7861ed78f840cc6878822b55b86272baa
Ben Robinson <benjaminrobinson@users.noreply.github.com>
2016-08-09 16:32:00 -0400

    Change "eligible" to "Eligible"

--- R/GetSector.R
+++ R/GetSector.R
@@ -51,7 +51,7 @@ GetSector <- function(exhibit){
                       "mu7"="Multiracial",
                       "pi7"="Pacific Islander",
                       "am7"="American Indian",
-                      "direct cert"="TANF/SNAP eligible",
+                      "direct cert"="TANF/SNAP Eligible",
                       "economy"="Economically Disadvantaged",
                       "lep"="English Learner",
                       "sped"="Special Education",
